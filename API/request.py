999b7924c04918a2c37a40b2386b5a958828c0a7
Brenno <brennoprogmm@gmail.com>
2025-12-11 10:02:35 -0300

    Ajuste mensagens LOG

--- API/request.py
+++ API/request.py
@@ -2,7 +2,7 @@ import requests
 import logging
 
 def request_data() -> dict:
-    url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados?formato=json;dataInicial=01/01/2025;dataFinal=31/12/2025&dataInicial=10/12/2015&dataFinal=10/12/2025"
+    url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados?formato=json;dataInicial=01/01/2025;dataFinal=31/12/2025&dataInicial=01/01/2025&dataFinal=31/12/2025"
     try:
         response = requests.get(url)
         response.raise_for_status()
--- main.py
+++ main.py
@@ -7,17 +7,18 @@ from API.save_file import save_as_json
 logging.basicConfig(filename='log.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s') 
 
 def main():
-    c_date = str(datetime.datetime.now())[:10]
-    api_data_path = f"src/data_{c_date}.json"
+    logging.info("INICIO de execucao")
+    day = str(datetime.datetime.now())[:10]
+    api_json_path = f"src/data_{day}.json"
     
-    if not today_data_exists(api_data_path):
+    if not today_data_exists(api_json_path):
         raw_data = request_data()
-        save_as_json(api_data_path, raw_data)
+        save_as_json(api_json_path, raw_data)
     else:
-        logging.INFO("Requisição API dispensada")
-    
+        logging.info("Requisicao API dispensada")
+
 
     print("Continuou")
-    
+    logging.info("FIM de execucao")
 if __name__ == "__main__":
     main()
