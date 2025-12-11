bf7470ddc15c0903f53a818a3fa34489719c199f
Brenno <brennoprogmm@gmail.com>
2025-12-11 09:31:34 -0300

    Reequisição modular API

--- API/request.py
+++ API/request.py
@@ -1,22 +1,16 @@
-import json
 import requests
-import datetime
-import os
-import csv
+import logging
 
-def request_data(file_path: str):
+def request_data() -> dict:
     url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados?formato=json;dataInicial=01/01/2025;dataFinal=31/12/2025&dataInicial=10/12/2015&dataFinal=10/12/2025"
-    response = requests.get(url)
-    if response.status_code == 200: 
-        status = "Concluido"
-        data = response.json()
-        with open(file_path, "w") as f:
-            json.dump(data, f)
-    else:
-        status = "Erro de retorno API"
-        pass
-
-
-        
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        logging.info("Requisicao bem sucedida")
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error de API : {e}")
+        raise SystemExit(e)
+    
 if __name__ == "__main__":
     request_data()
